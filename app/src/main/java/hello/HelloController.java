c650b326e76eed8a02ecc24e12b88a49c708ab24
rehka9008918867 <123856607+rehka9008918867@users.noreply.github.com>
2023-06-27 07:41:52 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello devi from Spring Boot!";
+        return "Hello from Spring Boot1!";
     }   
 }
